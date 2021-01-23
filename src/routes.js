44bf9117e45a044cf996d21b9b720ada19aab93b
caique-dev <cpinheroca@gmail.com>
2021-01-22 23:52:12 -0300

    Criano página de criação de produto

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const productsController = require('./controllers/products')
 
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
     return res.render('layout')
+})
+
+routes.get('/products/create', productsController.create)
+
+routes.get('/add/create', (req, res) => {
+    return res.redirect('/products/create')
 })
 
 // error 404
